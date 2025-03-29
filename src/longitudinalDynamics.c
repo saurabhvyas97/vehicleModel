af02a9145d6755b259a76fb88327ff8b08409e25
svyas97 <sdmdvyas@gmail.com>
2025-03-29 23:17:16 +0000

    Update in longitudinal dynamics

--- src/longitudinalDynamics.c
+++ src/longitudinalDynamics.c
@@ -5,13 +5,11 @@
 void calculateWheelLoads_LongLT(LongitudinalDynamics *longDyn, TireInputs *tireInput)
 {
     //Calculate load transfer
-    float latLoadTransferFront      = g_vehicleParam.mass * g_vehicleParam.weightBiasFront * longDyn->longitudinalAcceleration * g_vehicleParam.cgHeight / g_vehicleParam.wheelbase;
-    float latLoadTransferRear       = g_vehicleParam.mass * (1 - g_vehicleParam.weightBiasFront) * longDyn->longitudinalAcceleration * g_vehicleParam.cgHeight / g_vehicleParam.wheelbase;
-
-    //Calculate normal forces acting on the tires
-    tireInput->normalForceFrontLeft   = (g_vehicleParam.mass * 9.81 * g_vehicleParam.weightBiasFront / 2) - latLoadTransferFront;
-    tireInput->normalForceFrontRight   = (g_vehicleParam.mass * 9.81 * g_vehicleParam.weightBiasFront / 2) + latLoadTransferFront;
-    tireInput->normalForceRearLeft    = (g_vehicleParam.mass * 9.81 * (1 - g_vehicleParam.weightBiasFront) / 2) - latLoadTransferRear;
-    tireInput->normalForceRearRight    = (g_vehicleParam.mass * 9.81 * (1 - g_vehicleParam.weightBiasFront) / 2) + latLoadTransferRear;
+    float longLoadTransfer      = g_vehicleParam.mass * longDyn->longitudinalAcceleration * g_vehicleParam.cgHeight / g_vehicleParam.wheelbase;
     
+    //Calculate normal forces acting on the tires
+    tireInput->normalForceFrontLeft     = (g_vehicleParam.mass * 9.81 * g_vehicleParam.weightBiasFront / 2) - longLoadTransfer;
+    tireInput->normalForceFrontRight    = (g_vehicleParam.mass * 9.81 * g_vehicleParam.weightBiasFront / 2) + longLoadTransfer;
+    tireInput->normalForceRearLeft      = (g_vehicleParam.mass * 9.81 * (1 - g_vehicleParam.weightBiasFront) / 2) - longLoadTransfer;
+    tireInput->normalForceRearRight     = (g_vehicleParam.mass * 9.81 * (1 - g_vehicleParam.weightBiasFront) / 2) + longLoadTransfer;
 }
